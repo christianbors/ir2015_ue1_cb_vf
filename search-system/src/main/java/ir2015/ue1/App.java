375e933e43dad203e870ab663626bfba552e7758
Christian Bors <christianbors@gmail.com>
2015-03-22 10:50:40 +0100

    add simple command line interface sample project
    Signed-off-by: Christian Bors <christianbors@gmail.com>

--- search-system/src/main/java/ir2015/ue1/App.java
+++ search-system/src/main/java/ir2015/ue1/App.java
@@ -2,17 +2,20 @@ package ir2015.ue1;
 
 import org.apache.commons.cli.Options;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Options opt = new Options();
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-        opt.addOption("t", false, "display current time");
-        System.out.println( "Hello World!" );
-    }
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class App {
+
+     public static void main(String[] args) {
+         new Cli(args).parse();
+     }
+
 }
