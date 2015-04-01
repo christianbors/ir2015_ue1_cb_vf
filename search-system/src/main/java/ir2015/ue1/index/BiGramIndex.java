a76609ddc495e887cb26f8e795239dbb28a1ec92
Christian Bors <christianbors@gmail.com>
2015-04-01 18:36:12 +0200

    prepare BiGramIndex
    Signed-off-by: Christian Bors <christianbors@gmail.com>

--- search-system/src/main/java/ir2015/ue1/index/BiGramIndex.java
+++ search-system/src/main/java/ir2015/ue1/index/BiGramIndex.java
@@ -1,16 +1,35 @@
 package ir2015.ue1.index;
 
 import ir2015.ue1.model.Newsgroup;
+import org.apache.lucene.analysis.ngram.NGramTokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.List;
-import java.util.SortedMap;
 
 /**
  * Created by christianbors on 26/03/15.
  */
 public class BiGramIndex {
 
-    public BiGramIndex(List<Newsgroup> documents) {
+    private CharTermAttribute charTermAttribute;
+    private NGramTokenizer bigramTokenizer;
 
+    public BiGramIndex(List<Newsgroup> documents) {
+        StringReader r = new StringReader(documents.get(0).getText());
+        bigramTokenizer = new NGramTokenizer(2, 2);
+        try {
+            this.charTermAttribute = bigramTokenizer.addAttribute(CharTermAttribute.class);
+            bigramTokenizer.setReader(r);
+            bigramTokenizer.reset();
+            System.out.println(bigramTokenizer.toString());
+//            while (bigramTokenizer.incrementToken()) {
+//                System.out.println(charTermAttribute.toString());
+//            }
+//            bigramTokenizer.clearAttributes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- search-system/src/test/java/ir2015/ue1/index/BiGramIndexTest.java
+++ search-system/src/test/java/ir2015/ue1/index/BiGramIndexTest.java
@@ -1,31 +1,55 @@
 package ir2015.ue1.index;
 
-import junit.framework.Test; 
-import junit.framework.TestSuite; 
-import junit.framework.TestCase; 
+import ir2015.ue1.model.Newsgroup;
+import ir2015.ue1.parser.NewsgroupTopicParser;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
 
-/** 
-* BiGramIndex Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>03/28/2015</pre> 
-* @version 1.0 
-*/ 
-public class BiGramIndexTest extends TestCase { 
-public BiGramIndexTest(String name) { 
-super(name); 
-} 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public void setUp() throws Exception { 
-super.setUp(); 
-} 
+/**
+ * BiGramIndex Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>03/28/2015</pre>
+ */
+public class BiGramIndexTest extends TestCase {
+    List<Newsgroup> docs;
 
-public void tearDown() throws Exception { 
-super.tearDown(); 
-}
+    public BiGramIndexTest(String name) {
+        super(name);
+    }
 
-public static Test suite() { 
-return new TestSuite(BiGramIndexTest.class); 
-} 
+    public void setUp() throws Exception {
+        super.setUp();
+        docs = new ArrayList<Newsgroup>();
+        String path = "../20_newsgroups_subset/alt.atheism";
+        File folder = new File(path);
+        System.out.println(folder.listFiles().length);
+        for (int i = 0; i < 10; ++i) {
+            File f = folder.listFiles()[i];
+//        for (File f : folder.listFiles()) {
+            if(f.isFile()) {
+                docs.add(new NewsgroupTopicParser().parse(path + "/" + f.getName()));
+            }
+        }
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testBiGramIndexInstance() throws Exception {
+        BiGramIndex test = new BiGramIndex(docs);
+        assert test.getClass().equals(BiGramIndex.class);
+    }
+
+    public static Test suite() {
+        return new TestSuite(BiGramIndexTest.class);
+    }
 } 
 
